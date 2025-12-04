c88b090415630469245713130b3d532c872ed628
ابراهيم ايمن عبدالعزيز زكي <124893046+Ibrahim-Ayman@users.noreply.github.com>
2025-12-04 08:30:29 +0200

    [680] [Time Beats: 73.89%] [Memory Beats: 5.25%] - LeetPush

--- Valid-Palindrome-II.py
+++ Valid-Palindrome-II.py
@@ -18,15 +18,18 @@
 18                    return True
 19                return False
 20            elif s[firp] == s[secp-1] and done == False: 
-21                print(done)
-22                firp +=1 
-23                secp -=2
-24                done = True
-25            elif s[firp+1] == s[secp] and done == False:
-26                firp += 2
-27                secp -=1
-28                done = True
-29            else : 
-30                return False
-31        return True
-32
+21                s_needed1 = s[firp+1:secp-1]
+22                s_needed1_test = list(reversed(s_needed1))
+23                if s_needed1 == s_needed1_test : 
+24                    return True
+25                return False
+26            elif s[firp+1] == s[secp] and done == False:
+27                s_needed2 = s[firp+2:secp]
+28                s_needed2_test = list(reversed(s_needed2))
+29                if s_needed2 == s_needed2_test : 
+30                    return True
+31                return False
+32            else : 
+33                return False
+34        return True
+35
