aeee1fd80a94ab22ea7c1d630666156cac064744
ابراهيم ايمن عبدالعزيز زكي <124893046+Ibrahim-Ayman@users.noreply.github.com>
2025-12-06 11:54:54 +0200

    [601] [Time Beats: 45.24%] - LeetPush

--- Human-Traffic-of-Stadium.sql
+++ Human-Traffic-of-Stadium.sql
@@ -1,16 +1,25 @@
-1-- Write your PostgreSQL query statement below
-2with same_id_diff 
-3AS (
-4    SELECT * , id - row_number() OVER() as id_diff
-5    FROM Stadium
-6    WHERE people > 99
-7)
-8
-9SELECT id , visit_date , people
-10FROM same_id_diff
-11WHERE id_diff in (
-12    SELECT id_diff 
-13    FROM same_id_diff
-14    GROUP BY id_diff
-15    having COUNT(id_diff) > 2
-16)
+1SELECT *
+2FROM Stadium 
+3WHERE id in (
+4SELECT s1.id
+5FROM Stadium s1 
+6INNER JOIN Stadium s2 
+7ON s1.id = s2.id - 1 AND s1.people > 99 AND s2.people > 99
+8INNER JOIN Stadium s3 
+9ON s1.id = s3.id - 2 AND s1.people > 99 AND s3.people > 99)
+10OR id IN (
+11    SELECT s2.id
+12FROM Stadium s1 
+13INNER JOIN Stadium s2 
+14ON s1.id = s2.id - 1 AND s1.people > 99 AND s2.people > 99
+15INNER JOIN Stadium s3 
+16ON s1.id = s3.id - 2 AND s1.people > 99 AND s3.people > 99)
+17
+18OR id IN (
+19    SELECT s3.id
+20FROM Stadium s1 
+21INNER JOIN Stadium s2 
+22ON s1.id = s2.id - 1 AND s1.people > 99 AND s2.people > 99
+23INNER JOIN Stadium s3 
+24ON s1.id = s3.id - 2 AND s1.people > 99 AND s3.people > 99)
+25
