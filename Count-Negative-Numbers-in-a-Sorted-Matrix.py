57e17fa795756116e94cd2a17ff7ab9adaddff09
ابراهيم ايمن عبدالعزيز زكي <124893046+Ibrahim-Ayman@users.noreply.github.com>
2025-12-28 11:33:44 +0200

    [1351] [Time Beats: 100.00%] [Memory Beats: 88.94%] - LeetPush

--- Count-Negative-Numbers-in-a-Sorted-Matrix.py
+++ Count-Negative-Numbers-in-a-Sorted-Matrix.py
@@ -2,8 +2,11 @@
 2    def countNegatives(self, grid):
 3        count = 0
 4        for arr in grid: 
-5            for n in arr : 
-6                if n < 0 : 
-7                    count+=1
-8        return count
-9        
+5            count_arr = 0
+6            for n in arr :                 
+7                if n < 0 : 
+8                    break
+9                count_arr+=1
+10            if count_arr != len(arr):
+11                count+=len(arr)-count_arr
+12        return count
