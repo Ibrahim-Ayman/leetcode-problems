842127d110ab92d19bc16cdca241537558bfd570
ابراهيم ايمن عبدالعزيز زكي <124893046+Ibrahim-Ayman@users.noreply.github.com>
2026-02-20 15:15:43 +0200

    [3465] [Time Beats: 45.80%] - LeetPush

--- Find-Products-with-Valid-Serial-Numbers.sql
+++ Find-Products-with-Valid-Serial-Numbers.sql
@@ -1,5 +1,8 @@
 1-- Write your PostgreSQL query statement below
-2SELECT * 
+2SELECT *
 3FROM products
-4WHERE description ~ '(?:(?<=^)|(?<=\s))SN\d{4}-\d{4}(?:(?=$)|(?=\s))'
-5ORDER BY product_id
+4WHERE description ~ '^SN[0-9]{4}-[0-9]{4} [a-zA-Z0-9 ]*$' 
+5    OR description ~ '^[a-zA-Z0-9 ]* SN[0-9]{4}-[0-9]{4} [a-zA-Z0-9 ]*$'
+6    OR description ~ '^[a-zA-Z0-9 ]* SN[0-9]{4}-[0-9]{4}$'
+7    OR description ~ '^SN[0-9]{4}-[0-9]{4}$'
+8ORDER BY product_id
