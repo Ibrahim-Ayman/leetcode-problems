d57287174c48004c2177318b6d9f64a06fe33862
ابراهيم ايمن عبدالعزيز زكي <124893046+Ibrahim-Ayman@users.noreply.github.com>
2026-06-16 19:22:27 +0300

    [1341] [Time Beats: 48.81%] - LeetPush

--- Movie-Rating.sql
+++ Movie-Rating.sql
@@ -1,20 +1,19 @@
-1(SELECT u.name AS results      
-2FROM MovieRating mov_rate
-3INNER JOIN Users u
-4ON mov_rate.user_id = u.user_id
-5GROUP BY u.name 
-6ORDER BY COUNT(*) DESC, u.name
+1(SELECT Users.name AS results
+2FROM MovieRating mv 
+3INNER JOIN Users 
+4ON mv.user_id = Users.user_id 
+5GROUP BY name
+6ORDER BY COUNT(*) DESC, name
 7LIMIT 1)
 8
 9UNION ALL
 10
-11(SELECT m.title  AS results      
-12FROM MovieRating mov_rate
-13INNER JOIN Movies m
-14ON mov_rate.movie_id = m.movie_id
-15WHERE DATE_PART('MONTH' , created_at) = 2 AND DATE_PART('YEAR' , created_at) = 2020 
-16GROUP BY m.title
-17ORDER BY AVG(rating) DESC , m.title
+11(SELECT Movies.title AS results
+12FROM MovieRating mv 
+13INNER JOIN Movies  
+14ON mv.movie_id = Movies.movie_id   
+15WHERE DATE_PART('MONTH' , created_at) = 2 AND DATE_PART('YEAR' , created_at) = 2020
+16GROUP BY title
+17ORDER BY AVG(mv.rating) DESC, MIN(title)
 18LIMIT 1
 19)
-20
