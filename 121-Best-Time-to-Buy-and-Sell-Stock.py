0d732f3c46234442172fb6905092fb55dbcc6be6
ابراهيم ايمن عبدالعزيز زكي <124893046+Ibrahim-Ayman@users.noreply.github.com>
2026-08-01 19:21:05 +0300

    [121] [Time Beats: 98.70%] [Memory Beats: 75.18%] - LeetPush

--- 121-Best-Time-to-Buy-and-Sell-Stock.py
+++ 121-Best-Time-to-Buy-and-Sell-Stock.py
@@ -1,13 +1,13 @@
 class Solution(object):
     def maxProfit(self, prices):
         min_price = float('inf')
-        max_price = 0 
+        max_profit = 0 
 
         for price in prices : 
             if price < min_price : 
                 min_price = price 
-            elif price - min_price > max_price : 
-                max_price = price - min_price
-        return max_price
+            elif price - min_price > max_profit : 
+                max_profit = price - min_price
+        return max_profit
 
         
