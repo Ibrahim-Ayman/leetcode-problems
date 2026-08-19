9de7ad9962e2341b6c5505912e8224a431c5d72a
ابراهيم ايمن عبدالعزيز زكي <124893046+Ibrahim-Ayman@users.noreply.github.com>
2026-08-19 11:50:00 +0300

    [262] [Time Beats: 18.12%] - LeetPush

--- 262-Trips-and-Users.sql
+++ 262-Trips-and-Users.sql
@@ -1,19 +1,17 @@
-# Write your MySQL query statement below
-select request_at as Day, round(avg(casee),2) as 'Cancellation Rate' 
-from 
-(
-select request_at , id , case when 
-    status = 'cancelled_by_driver' or status = 'cancelled_by_client' then 1.0 
-    else 0.0 
-    end as casee
-from trips 
-    inner join users user1
-        on client_id  = user1.users_id and user1.role = 'client'
-    inner join users user2
-        on driver_id  = user2.users_id and user2.role = 'driver'  
-where
-    request_at in ('2013-10-02' , '2013-10-03' , '2013-10-01')
-    and user1.banned = 'No'
-    and user2.banned = 'No'
-) as newtable
-group by request_at
+-- Write your PostgreSQL query statement below
+SELECT request_at AS Day , 
+    ROUND(
+    (CAST(
+    SUM(CASE WHEN status = 'cancelled_by_driver' 
+    THEN 1
+    WHEN status = 'cancelled_by_client' 
+    THEN 1
+    ELSE 0 END) AS FLOAT) / CAST(COUNT(*) AS FLOAT))::numeric
+    , 2) AS "Cancellation Rate"
+FROM Trips t
+INNER JOIN Users d 
+ON t.driver_id = d.users_id 
+INNER JOIN Users c 
+ON t.client_id = c.users_id 
+WHERE d.banned = 'No' and c.banned = 'No' and request_at in ('2013-10-01' , '2013-10-02' , '2013-10-03')
+GROUP BY request_at
