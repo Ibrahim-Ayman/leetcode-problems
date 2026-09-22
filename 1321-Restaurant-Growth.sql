4d556855c5c23778abe1a9dfabc5c45d6eaea039
ابراهيم ايمن عبدالعزيز زكي <124893046+Ibrahim-Ayman@users.noreply.github.com>
2026-09-22 10:30:30 +0300

    [1321] [Time Beats: 66.88%] - LeetPush

--- 1321-Restaurant-Growth.sql
+++ 1321-Restaurant-Growth.sql
@@ -1,7 +1,14 @@
-select  visited_on, amount , average_amount 
-from 
-    (select visited_on, sum(sum(amount)) over(order by visited_on RANGE BETWEEN INTERVAL 6 DAY   PRECEDING AND CURRENT Row) as amount , round(avg(sum(amount)) over(order by visited_on RANGE BETWEEN INTERVAL 6 DAY   PRECEDING AND CURRENT Row) , 2) as average_amount , row_number() over(order by visited_on) as rn
-    from customer
-    group by visited_on
-    order by visited_on  ) as newtable
-where rn > 6 
+SELECT visited_on , amount , average_amount
+FROM (
+SELECT visited_on 
+        , SUM(daily_amount) 
+        OVER(ORDER BY visited_on RANGE BETWEEN '6 DAY' PRECEDING AND CURRENT ROW) AS amount
+        , ROUND(AVG(daily_amount) 
+        OVER(ORDER BY visited_on RANGE BETWEEN '6 DAY' PRECEDING AND CURRENT ROW) , 2) AS average_amount 
+        , visited_on - MIN(visited_on) OVER() AS days_start
+FROM (
+    SELECT visited_on , SUM(amount) AS daily_amount 
+    FROM Customer
+    GROUP BY visited_on
+))
+WHERE days_start >= 6
